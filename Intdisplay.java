ec85ec567b11635d70b9f779f381f0a1b3351ed3
Sree divya <160315585+artsbysree@users.noreply.github.com>
2025-11-08 14:44:54 +0530

    Create Intdisplay.java
    java program to enter a number

---
+++ Intdisplay.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Racer {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the speed of 5 racers: ");
+		int racer1 = reader.nextInt();
+		System.out.println("You Entered : " +number);
+	}
+}
